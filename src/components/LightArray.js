c9cbc2b9464804a7c1ee0dde414373d23b3b8e5d
Timothy Hwang <thwang@rocketfuel.com>
2017-12-24 19:02:03 -0800

    Split lights to <LightArray/>

---
+++ src/components/LightArray.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  PointLight,
+  View,
+} from 'react-vr';
+
+import hidable from '../decorators/hidable';
+
+const LightArray = () => (
+  <View>
+    <PointLight
+      style={{
+        color: 'green',
+        transform: [
+          { translate: [10, 0, 0] }
+        ],
+      }}
+    />
+    <PointLight
+      style={{
+        color: 'blue',
+        transform: [
+          { translate: [-10, 0, 0] }
+        ],
+      }}
+    />
+    <PointLight
+      style={{
+        color: 'red',
+        transform: [
+          { translate: [0, 0, -10] }
+        ],
+      }}
+    />
+    <PointLight
+      style={{
+        color: 'red',
+        transform: [
+          { translate: [0, 0, 10] }
+        ],
+      }}
+    />
+    <PointLight
+      style={{
+        color: 'orange',
+        transform: [
+          { translate: [0, 10, 0] }
+        ],
+      }}
+    />
+  </View>
+);
+
+export default hidable(LightArray);
